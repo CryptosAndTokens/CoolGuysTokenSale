99a8fa01be12e2a737c3d6a1d29f557a06751c64
Robert Aronovici <robert@robertaronovici.com>
2018-09-20 12:56:29 -0700

    Create a token sale contract with testing methods

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var CoolGuyToken = artifacts.require("./CoolGuyToken.sol");
+var CoolGuyTokenSale = artifacts.require("./CoolGuyTokenSale.sol")
 
 module.exports = function(deployer) {
-  deployer.deploy(CoolGuyToken, 1000000);
+  deployer.deploy(CoolGuyToken, 1000000).then(function(){
+  	// Token price is 0.001 Ether
+  	var tokenPrice = 1000000000000000 // in wei
+  	return deployer.deploy(CoolGuyTokenSale, CoolGuyToken.address, tokenPrice);
+  });
 };
